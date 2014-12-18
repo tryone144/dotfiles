8c8b50500329e71e60c5b785bf5b313024df177d
Bernd Busse <bernd@busse-net.de>
2014-12-19 00:22:10 +0100

    dmenu-dialog to poweroff or quit i3

---
+++ i3/.config/i3/scripts/poweroff.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#
+# I3WM
+# Helper to show a shutdown selection with dmenu
+#   needs: [dmenu2]
+#
+# file: ~/.config/i3/scripts/poweroff.sh
+# v0.1 / 2014.12.18
+#
+# (c) 2014 Bernd Busse
+#
+
+screen_geometry="$(xrandr -d "${DISPLAY}" --query | grep ' connected ' | sed -n '1p' | grep -o -e '[0-9]\+x[0-9]\+')"
+screen_width="$(echo "${screen_geometry}" | cut -d'x' -f1)"
+screen_height="$(echo "${screen_geometry}" | cut -d'x' -f2)"
+
+lines_num=5
+lines_height=26
+height=$((${lines_num} *${lines_height}))
+width=640
+
+value="$(/usr/bin/dmenu -f -i -nb '#1A1A1A' -nf '#BEBEBE' -sb '#1793D1' -sf '#FFFFFF' -dim 0.5 \
+    -l ${lines_num} -h ${lines_height} -x $(((${screen_width} - ${width}) / 2)) -y $(((${screen_height} - ${height}) / 2)) -w ${width} -fn 'Ubuntu Mono-16:normal' <<EOF
+> poweroff
+> suspend
+> lock screen
+> reload i3
+> quit i3
+EOF
+)"
+
+# check choosen value
+case "${value:2}" in
+    "poweroff") # shutdown computer
+        systemctl poweroff
+        ;;
+    "suspend") # suspend computer
+        systemctl suspend
+        ;;
+    "lock screen") # lock screen
+        ${I3_CONFIG}/scripts/screenlock.sh
+        ;;
+    "reload i3") # reload i3
+        i3-msg reload
+        ;;
+    "quit i3") # quit i3
+        i3-msg exit
+        ;;
+    *) # aborted, do nothing
+        ;;
+esac
+
