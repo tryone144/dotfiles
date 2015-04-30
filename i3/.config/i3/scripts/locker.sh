f6852725edd158ee4f2dccf630eb6f563b35874e
Bernd Busse <bernd@busse-net.de>
2015-04-30 15:27:09 +0200

    lockscreen: mute audio on lock, unmute on unlock

--- i3/.config/i3/scripts/locker.sh
+++ i3/.config/i3/scripts/locker.sh
@@ -4,15 +4,29 @@
 # Helper to lock screen (and do the related stuff)
 #   needs: [sxlock]
 #
-# file: ~/.config/i3/scripts/screenlock.sh
-# v1.0 / 2014.12.24
+# file: ~/.config/i3/scripts/locker.sh
+# v1.1 / 2015.04.30
 #
-# (c) 2014 Bernd Busse
+# (c) 2015 Bernd Busse
 #
 
-killall -SIGUSR1 dunst # pause dunst
+# pause dunst notifications
+killall -SIGUSR1 dunst
 
+# mute audio
+is_muted=$(${I3_CONFIG}/scripts/volume.py get | grep -v "muted" &> /dev/null; echo ${?})
+${I3_CONFIG}/scripts/volume.py mute
+
+# lock screen
 sxlock -p 'password1234' -u "${USER}@${HOSTNAME}" -f '-*-ubuntu mono-medium-r-normal-*-24-90-*-*-*-*-iso10646-*'
 
-killall -SIGUSR2 dunst # resume dunst
+############################################################
+
+# resume dunst notifications
+killall -SIGUSR2 dunst
+
+# unmute audio
+if (( $is_muted == 0 )); then
+    ${I3_CONFIG}/scripts/volume.py unmute
+fi
 
