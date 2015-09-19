d5aa0a5cf512cef2966aee3d1e25c5c1a6b56901
Bernd Busse <bernd@busse-net.de>
2015-09-19 21:52:11 +0200

    updated volume changer script

--- i3/.config/i3/scripts/volume.py
+++ i3/.config/i3/scripts/volume.py
@@ -7,7 +7,7 @@
 #          [alsa]
 #
 # file: ~/.config/i3/scripts/volume.py
-# v0.4 / 2015.04.30
+# v0.6 / 2015.07.19
 #
 # (c) 2015 Bernd Busse
 #
@@ -17,119 +17,170 @@ import re
 import sys
 import subprocess
 
-amixer_cmd = ['amixer', '-D', 'default', '--']
-pactl_cmd = ['pactl', '--']
+
+class sink(object):
+    """ Generic sink. To be subclassed... """
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        out = "{0}%".format(self.get_volume())
+        if self.is_mute():
+            out += " (muted)"
+
+        return out
+
+    def set_volume(self, level):
+        raise NotImplementedError
+
+    def lower_volume(self, step=10):
+        raise NotImplementedError
+
+    def raise_volume(self, step=10):
+        raise NotImplementedError
+
+    def get_volume(self):
+        raise NotImplementedError
+
+    def set_mute(self, enabled):
+        raise NotImplementedError
+
+    def toggle_mute(self):
+        raise NotImplementedError
+
+    def is_mute(self):
+        raise NotImplementedError
+
+    def call(self, cmd):
+        if subprocess.call(cmd, stdout=subprocess.DEVNULL) != 0:
+            sys.stderr.write("Fehler aufgetreten\n")
+            sys.exit(1)
 
 
-# mute
-def mute_onoff(mute=True):
-    pa_sink = get_sink()
-    if pa_sink is not None:
-        cmd = pactl_cmd + ['set-sink-mute',
-                           str(pa_sink), str(mute).lower()]
-    else:
-        cmd = amixer_cmd + ['sset', 'Master',
-                            'playback', 'mute' if mute else 'unmute']
-    if subprocess.call(cmd, stdout=subprocess.DEVNULL) != 0:
-        sys.stderr.write("Fehler aufgetreten\n")
+class alsa_sink(sink):
+    """ ALSA specific actions for device with name __name__ """
+    def __init__(self, name='default'):
+        super().__init__()
 
+        self.name = name
+        self.cmd = ['amixer', '-D', self.name, '--']
 
-# toggle mute
-def mute_toggle():
-    pa_sink = get_sink()
-    if pa_sink is not None:
-        cmd = pactl_cmd + ['set-sink-mute',
-                           str(pa_sink), 'toggle']
-    else:
-        cmd = amixer_cmd + ['sset', 'Master',
-                            'playback', 'toggle']
-    if subprocess.call(cmd, stdout=subprocess.DEVNULL) != 0:
-        sys.stderr.write("Fehler aufgetreten\n")
+    def set_volume(self, level):
+        cmd = self.cmd + ['sset', 'Master', 'playback',
+                          '{0}%'.format(level)]
+        self.call(cmd)
 
+    def lower_volume(self, step=10):
+        cmd = self.cmd + ['sset', 'Master', 'playback',
+                          '{0}%-'.format(step)]
+        self.call(cmd)
 
-# lower volume by given percent
-def lower_volume(step=10):
-    pa_sink = get_sink()
-    if pa_sink is not None:
-        cmd = pactl_cmd + ['set-sink-volume',
-                           str(pa_sink), '-{0}%'.format(step)]
-    else:
-        cmd = amixer_cmd + ['sset', 'Master',
-                            'playback', '{0}%-'.format(step)]
-    if subprocess.call(cmd, stdout=subprocess.DEVNULL) != 0:
-        sys.stderr.write("Fehler aufgetreten\n")
+    def raise_volume(self, step=10):
+        cmd = self.cmd + ['sset', 'Master', 'playback',
+                          '{0}%+'.format(step)]
+        self.call(cmd)
 
-
-# raise volume by given percent
-def raise_volume(step=10):
-    pa_sink = get_sink()
-    if pa_sink is not None:
-        if (int(re.search('^([0-9]+)%.*$', get_volume()).group(1)) + step) > 100:
-            cmd = pactl_cmd + ['set-sink-volume',
-                               str(pa_sink), '100%']
-        else:
-            cmd = pactl_cmd + ['set-sink-volume',
-                               str(pa_sink), '+{0}%'.format(step)]
-    else:
-        cmd = amixer_cmd + ['sset', 'Master',
-                            'playback', '{0}%+'.format(step)]
-    if subprocess.call(cmd, stdout=subprocess.DEVNULL) != 0:
-        sys.stderr.write("Fehler aufgetreten\n")
-
-
-# is muted
-def is_muted():
-    pa_sink = get_sink()
-    if pa_sink is not None:
-        cmd = pactl_cmd + ['list', 'sinks']
+    def get_volume(self):
+        cmd = self.cmd + ['sget', 'Master', 'playback']
         out = str(subprocess.check_output(cmd), "utf-8")
-        sinks = re.findall('^\tMute:\s(\w+)$', out, re.M)
-        mute = re.match('yes', sinks[pa_sink])
-    else:
-        cmd = amixer_cmd + ['sget', 'Master', 'playback']
+        level = re.search('(?!\n).*\[([0-9]+)%\].*\n', out).group(1)
+
+        return int(level)
+
+    def set_mute(self, mute):
+        cmd = self.cmd + ['sset', 'Master', 'playback',
+                          'mute' if mute else 'unmute']
+        self.call(cmd)
+
+    def toggle_mute(self):
+        cmd = self.cmd + ['sset', 'Master', 'playback',
+                          'toggle']
+        self.call(cmd)
+
+    def is_mute(self):
+        cmd = self.cmd + ['sget', 'Master', 'playback']
         out = str(subprocess.check_output(cmd), "utf-8")
         mute = re.search('\[off\]\n', out)
 
-    if mute is not None:
-        return True
-    else:
-        return False
+        return mute is not None
 
 
-# get volume level in percent
-def get_volume():
-    pa_sink = get_sink()
-    if pa_sink is not None:
-        cmd = pactl_cmd + ['list', 'sinks']
+class pulseaudio_sink(sink):
+    """ PulseAudio specific actions for sink with index __index__ """
+    def __init__(self, index, number):
+        super().__init__()
+
+        self.number = number
+        self.index = index
+        self.cmd = ['pactl', '--']
+
+    def set_volume(self, level):
+        if level > 100:
+            print("Level {0}% is larger than 100%, resetting...".format(level))
+            level = 100
+
+        cmd = self.cmd + ['set-sink-volume', str(self.index),
+                          '{0}%'.format(level)]
+        self.call(cmd)
+
+    def lower_volume(self, step=10):
+        cmd = self.cmd + ['set-sink-volume', str(self.index),
+                          '-{0}%'.format(step)]
+        self.call(cmd)
+
+    def raise_volume(self, step=10):
+        cmd = self.cmd + ['set-sink-volume', str(self.index),
+                          '+{0}%'.format(step)]
+        self.call(cmd)
+
+    def get_volume(self):
+        cmd = self.cmd + ['list', 'sinks']
         out = str(subprocess.check_output(cmd), "utf-8")
-        levels = re.findall('^\tVolume:.*\w+:\s*[0-9]+\s*/\s*([0-9]+%)\s*/.*$', out, re.M)
-        level = levels[pa_sink]
-    else:
-        cmd = amixer_cmd + ['sget', 'Master', 'playback']
-        out = str(subprocess.check_output(cmd), "utf-8")
-        level = re.search('(?!\n).*\[([0-9]+%)\].*\n', out).group(1)
+        levels = re.findall('^\tVolume:\s*[\w-]+:\s*[0-9]+\s*\/\s*([0-9]+)%',
+                            out, re.M)
 
-    if is_muted():
-        level += " (muted)"
-    return level
+        return int(levels[self.number])
+
+    def set_mute(self, mute):
+        cmd = self.cmd + ['set-sink-mute', str(self.index),
+                          'true' if mute else 'false']
+        self.call(cmd)
+
+    def toggle_mute(self):
+        cmd = self.cmd + ['set-sink-mute', str(self.index),
+                          'toggle']
+        self.call(cmd)
+
+    def is_mute(self):
+        cmd = self.cmd + ['list', 'sinks']
+        out = str(subprocess.check_output(cmd), "utf-8")
+        sinks = re.findall('^\tMute:\s(\w+)$', out, re.M)
+        mute = re.match('yes', sinks[self.number])
+
+        return mute is not None
 
 
 # test for 'pactl'
-def get_sink():
-    if subprocess.call(["pactl"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 127:
-        return None
+def get_sinks():
+    if subprocess.call(["pactl"], stdout=subprocess.DEVNULL,
+                       stderr=subprocess.DEVNULL) == 127:
+        # no PulseAudio support, fallback to ALSA
+        return [alsa_sink(name="default")]
     else:
-        cmd = pactl_cmd + ['list', 'sinks', 'short']
-        out = str(subprocess.check_output(cmd), "utf-8")
-        sink = re.search('^([0-9])\t.*\tRUNNING$', out, re.M)
-        if sink is not None:
-            return int(sink.group(1))
-        else:
-            sinks = re.finditer('^([0-9])\t(\w+)\t.*$', out, re.M)
-            for s in sinks:
-                if not re.match('null', s.group(2), re.I):
-                    return int(s.group(1))
-            return None
+        sink_list = []
+
+        cmd = ['pactl', '--', 'list', 'sinks', 'short']
+        out = str(subprocess.check_output(cmd), "utf-8").splitlines()
+
+        for i in range(len(out)):
+            sink = re.search('^([0-9])\t.*\t(.+)\t.*\tRUNNING$', out[i], re.M)
+            if sink is not None:
+                if not re.search('null', sink.group(2), re.I) \
+                        and not re.search('equalizer', sink.group(2), re.I):
+                    sink_list.append(pulseaudio_sink(int(sink.group(1)), i))
+
+        return sink_list
+
 
 commands = ['mute',   'm',
             'unmute', 'u',
@@ -158,37 +209,37 @@ def main():
         sys.exit(1)
 
     action = args[1]
-    if action == 'toggle' or action == 't':
-        mute_toggle()
-        if verbose:
-            print("toggle mute ==> {0}".format(get_volume()))
-    elif action == 'mute' or action == 'm':
-        mute_onoff(mute=True)
-        if verbose:
-            print("mute on ==> {0}".format(get_volume()))
-    elif action == 'unmute' or action == 'u':
-        mute_onoff(mute=False)
-        if verbose:
-            print("mute off ==> {0}".format(get_volume()))
-    elif action == 'lower' or action == 'l':
-        try:
-            step = int(args[2])
-        except (IndexError, ValueError):
-            step = 10
-        lower_volume(step)
-        if verbose:
-            print("lower volume by {1}% ==> {0}".format(get_volume(), step))
-    elif action == 'raise' or action == 'r':
-        try:
-            step = int(args[2])
-        except (IndexError, ValueError):
-            step = 10
-        raise_volume(step)
-        if verbose:
-            print("raise volume by {1}% ==> {0}".format(get_volume(), step))
-    elif action == 'get' or action == 'g':
-        print(get_volume())
+    for s in get_sinks():
+        if action == 'toggle' or action == 't':
+            s.toggle_mute()
+            if verbose:
+                print("toggle mute ==> {0}".format(s))
+        elif action == 'mute' or action == 'm':
+            s.set_mute(mute=True)
+            if verbose:
+                print("mute on ==> {0}".format(s))
+        elif action == 'unmute' or action == 'u':
+            s.set_mute(mute=False)
+            if verbose:
+                print("mute off ==> {0}".format(s))
+        elif action == 'lower' or action == 'l':
+            try:
+                step = int(args[2])
+            except (IndexError, ValueError):
+                step = 10
+            s.lower_volume(step)
+            if verbose:
+                print("lower volume by {1}% ==> {0}".format(s, step))
+        elif action == 'raise' or action == 'r':
+            try:
+                step = int(args[2])
+            except (IndexError, ValueError):
+                step = 10
+            s.raise_volume(step)
+            if verbose:
+                print("raise volume by {1}% ==> {0}".format(s, step))
+        elif action == 'get' or action == 'g':
+            print(s)
 
 if __name__ == '__main__':
     main()
-
