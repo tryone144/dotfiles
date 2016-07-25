cb6b0746a5730464b976e3fcd538587a3e399dad
Bernd Busse <bernd@busse-net.de>
2016-07-25 18:19:24 +0200

    fixed beamer config, now supports more outputs

--- i3/.config/i3/scripts/beamer.sh
+++ i3/.config/i3/scripts/beamer.sh
@@ -5,37 +5,107 @@
 #   needs: [xrandr]
 #
 # file: ~/.config/i3/scripts/beamer.sh
-# v0.1 / 2016.02.04
+# v1.0 / 2016.07.25
 #
 # (c) 2016 Bernd Busse
 #
 
-# commands        
-EXTERNAL_ONLY="xrandr --output eDP1 --off --output DP2-2 --preferred --right-of eDP1 --primary"
-INTERNAL_ONLY="xrandr --output eDP1 --preferred --primary --output DP2-2 --off"
-BOTH="xrandr --output eDP1 --preferred --primary --output DP2-2 --preferred --right-of eDP1"
+INTERNAL='eDP1'
+EXTERNALS='DP2-1 DP2-2 DP2-3 HDMI1 HDMI2 DP1 DP2'
 
-# test for external screen
-is_connected=$(xrandr | grep -E '^DP2-2\s+' | grep -E '\s+connected\s+' &> /dev/null; echo ${?})
+# backend commands
+function enable_internal() {
+    xrandr --output "${INTERNAL}" --preferred
+}
+function enable_external() {
+    xrandr --output "${1}" --preferred --right-of "${INTERNAL}"
+}
+function disable_internal() {
+    xrandr --output "${INTERNAL}" --off
+}
+function disable_external_all() {
+    for ext in ${EXTERNALS}; do
+        xrandr --output "${ext}" --off
+    done
+}
+function set_primary() {
+    xrandr --output "${1}" --primary
+}
 
-if (( ${is_connected} == 0 )); then
-    # screen is connected
-    internal_active=$(xrandr | grep -E '^eDP1\s+' | grep -E '\s+connected\s+.*[0-9]+.*\(' &> /dev/null; echo ${?})
-    external_active=$(xrandr | grep -E '^DP2-2\s+' | grep -E '\s+connected\s+.*[0-9]+.*\(' &> /dev/null; echo ${?})
-    if [[ "${1}" == "external" ]]; then
-        ${EXTERNAL_ONLY}
-    elif [[ "${1}" == "internal" ]]; then
-        ${INTERNAL_ONLY}
-    else
-        if (( ${internal_active} == 0 && ${external_active} == 0 )); then
-            ${EXTERNAL_ONLY}
-        else if (( ${internal_active} != 0 && ${external_active} == 0 )) || [[ "${1}" == "internal" ]]; then
-            ${INTERNAL_ONLY}
-        else
-            ${BOTH}
-        fi
-        fi
-    fi
-    i3-msg -q -t command restart
+# helper commands
+function internal_only() {
+    echo "Enable internal / disable external"
+    enable_internal
+    set_primary "${INTERNAL}"
+    disable_external_all
+}
+function external_only() {
+    echo "Disable internal / enable external '${1}'"
+    enable_external "${1}"
+    set_primary "${1}"
+    disable_internal
+}
+function both() {
+    echo "Enable internal / enable external '${connected_screen}'"
+    enable_internal
+    set_primary "${INTERNAL}"
+    enable_external "${1}"
+}
+
+function i3_reload() {
+    killall compton
+    sleep 0.5; i3-msg -q -t command restart
+}
+
+# check for 'internal' argument
+if [[ "${1}" == 'internal' ]]; then
+    internal_only
+    i3_reload
+    exit
 fi
 
+# test for external screen
+is_connected=0
+connected_screen=""
+for screen in ${EXTERNALS}; do
+    connected=$( xrandr | grep -E "^${screen}\s+" | grep -E '\s+connected\s+' &> /dev/null; echo ${?} )
+    if (( ${connected}  == 0 )); then
+        echo "Found connected monitor: ${screen}"
+        is_connected=1
+        connected_screen="${screen}"
+        break
+    fi
+done
+
+# external screen is connected?
+if (( ${is_connected} )); then
+    # check for 'external' and 'both' argument
+    if [[ "${1}" == 'external' ]]; then
+        external_only "${connected_screen}"
+        i3_reload
+        exit
+    elif [[ "${1}" == 'both' ]]; then
+        both "${connected_screen}"
+        i3_reload
+        exit
+    fi
+
+    # check active screens
+    internal_active=$( xrandr | grep -E "^${INTERNAL}\s+" | grep -E '\s+connected\s+.*[0-9]+.*\(' &> /dev/null; echo ${?} )
+    external_active=$( xrandr | grep -E "^${connected_screen}\s+" | grep -E '\s+connected\s+.*[0-9]+.*\(' &> /dev/null; echo ${?} )
+
+    # toggle screens (both -> external -> internal)
+    if (( ${internal_active} == 0 && ${external_active} == 0 )); then
+        # both active
+        external_only "${connected_screen}"
+    elif (( ${internal_active} != 0 && ${external_active} == 0 )); then
+        # external only
+        internal_only
+    else
+        # internal only
+        both "${connected_screen}"
+    fi
+
+    i3_reload
+    exit
+fi
