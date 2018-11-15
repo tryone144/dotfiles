2854fd7d57ee257e9e4650a3e844549df13ae044
Bernd Busse <bernd@busse-net.de>
2018-11-15 22:05:58 +0100

    Compton/sxlock: enable fading of lockscreen

--- wmscripts/.config/wmscripts/locker.sh
+++ wmscripts/.config/wmscripts/locker.sh
@@ -11,15 +11,35 @@
 #
 
 has_backlight="$( which xbacklight 2>/dev/null )"
+dpy="${DISPLAY/:/_}"
+
+service="com.github.chjj.compton.${dpy}"
+interface='com.github.chjj.compton'
+object='/com/github/chjj/compton'
+
+opt_openclose="$( dbus-send --print-reply=literal --dest="${service}" "${object}"
+    "${interface}.opts_get" string:no_fading_openclose | awk '{print $2;}' )"
+opt_unredir="$( dbus-send --print-reply=literal --dest="${service}" "${object}"
+    "${interface}.opts_get" string:unredir_if_possible | awk '{print $2;}' )"
+
+# Enable compton's fade-in effect so that the lockscreen gets a nice fade-in effect.
+# If disable unredir_if_possible is enabled in compton's config, we may need to
+# disable that to avoid flickering. YMMV.
+dbus-send --type=method_call --dest="${service}" "${object}" "${interface}.opts_set" \
+    "string:no_fading_openclose" "boolean:false"
+dbus-send --type=method_call --dest="${service}" "${object}" "${interface}.opts_set" \
+    "string:unredir_if_possible" "boolean:false"
 
 # pause dunst notifications
-killall -SIGUSR1 dunst
+pkill -u ${USER} -USR1 dunst
 
 # dim screen
 if [[ -n "${has_backlight}" ]]; then
     back_val="$(( $(xbacklight -get | cut -d '.' -f 1) + 1))"
-    xbacklight -time 500 -steps 50 -set 1
+    xbacklight -time 500 -steps 50 -set 1 &
 fi
+
+sleep 0.2
 
 # mute audio
 #is_muted=$(${I3_CONFIG}/scripts/volume.py get | grep -v "muted" &> /dev/null; echo ${?})
@@ -32,12 +52,20 @@ sxlock -p 'password1234' -u "${USER}@${HOSTNAME}" -f '-*-ubuntu mono-medium-r-no
 ############################################################
 
 # resume dunst notifications
-killall -SIGUSR2 dunst
+pkill -u ${USER} -USR2 dunst
 
 # undim screen
 if [[ -n "${has_backlight}" ]]; then
     xbacklight -time 500 -steps 50 -set ${back_val} &
 fi
+
+sleep 0.2
+
+# Revert compton's config changes.
+dbus-send --type=method_call --dest="${service}" "${object}" "${interface}.opts_set" \
+    "string:no_fading_openclose" "boolean:${opt_openclose}"
+dbus-send --type=method_call --dest="${service}" "${object}" "${interface}.opts_set" \
+    "string:unredir_if_possible" "boolean:${opt_unredir}"
 
 # unmute audio
 #if (( $is_muted == 0 )); then
