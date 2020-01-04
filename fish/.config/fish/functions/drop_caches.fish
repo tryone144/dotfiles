2087a223c7f7ea67ba89912be8f98b7a84ff79e6
Bernd Busse <bernd@busse-net.de>
2020-01-04 04:55:12 +0100

    fish: drop_cache alias: sync before dropping

--- fish/.config/fish/functions/drop_caches.fish
+++ fish/.config/fish/functions/drop_caches.fish
@@ -1,3 +1,4 @@
 function drop_caches --description 'alias drop_caches=echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null'
+    sudo sync
     echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null;
 end
