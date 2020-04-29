0df607d789a0c891bf829bf1c4f804923af2944c
Bernd Busse <bernd@busse-net.de>
2020-04-29 17:26:58 +0200

    mediactl: Add support to mpris interface in VLC

--- wmscripts/.local/share/wmscripts/mediactl.sh
+++ wmscripts/.local/share/wmscripts/mediactl.sh
@@ -3,10 +3,10 @@
 # WMScripts
 # Helper to toggle play status of MPD / mpris
 #   needs: [mpc] / [mpd]
-#          [plasma-browser-integration]
+#          [qdbus] + ([plasma-browser-integration] || [vlc])
 #
 # file: ~/.local/share/wmscripts/mediactl.sh
-# v0.2 / 2020.02.06
+# v0.3 / 2020.04.29
 #
 # (c) 2020 Bernd Busse
 #
@@ -14,8 +14,9 @@
 __CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 __LAST_CLIENT="$( realpath "$__CONFIG_HOME/mediactl_status.txt" )"
 
+
 function usage() {
-    echo "usage: ${0} [mpd|mpris] {toggle|stop|prev|next}" >&2
+    echo "usage: ${0} [mpd|vlc|plasma] {toggle|stop|prev|next}" >&2
     exit 1
 }
 
@@ -26,9 +27,15 @@ function save_last_client() {
     echo "$1" > "$__LAST_CLIENT" 2>/dev/null
 }
 
-# call plasma-mpris dbus interface
+
+MPRIS_PLASMA="org.kde.plasma.browser_integration"
+MPRIS_VLC="org.mpris.MediaPlayer2.vlc"
+
+# call mpris MediaPlaye2 dbus interface
 function mpris_call() {
-    qdbus "org.kde.plasma.browser_integration" "/org/mpris/MediaPlayer2" "org.mpris.MediaPlayer2.$1"
+    local service="${1}"
+    local method="${2}"
+    qdbus "${service}" "/org/mpris/MediaPlayer2" "org.mpris.MediaPlayer2.${method}"
 }
 
 
@@ -44,7 +51,7 @@ function mpd_isrunning() {
 
 # check if mpd is playing
 function mpd_playing() {
-    stats="$( mpc 2>/dev/null )"
+    local stats="$( mpc 2>/dev/null )"
     [[ $( echo "$stats" | wc -l ) -gt 1 ]] && echo "$stats" | head -n 2 | tail -n 1 | grep -e '^\[playing\]' >/dev/null
     if [[ $? -eq 0 ]]; then
         return 0
@@ -63,35 +70,61 @@ function mpd_canplay() {
 }
 
 
-# check if plasma-mpris is running
+# check if mpris MediaPlayer2 is running
 function mpris_isrunning() {
-    qdbus | grep -e "^\s*org\.kde\.plasma\.browser_integration" 2>&1 >/dev/null
+    local service="${1//./\\.}"
+    qdbus | grep -e "^\s*${service}" 2>&1 >/dev/null
     if [[ $? -eq 0 ]]; then
         return 0
     fi
 
     return 1
 }
+function plasma_isrunning() {
+    mpris_isrunning "${MPRIS_PLASMA}"
+}
+function vlc_isrunning() {
+    mpris_isrunning "${MPRIS_VLC}"
+}
 
-# check if plasma-mpris is playing
+# check if mpris MediaPlayer2 is playing
 function mpris_playing() {
-    reply="$( mpris_call "Player.PlaybackStatus" 2>/dev/null )"
+    service="${1}"
+    reply="$( mpris_call "${service}" "Player.PlaybackStatus" 2>/dev/null )"
     if [[ "$reply" == "Playing" ]]; then
         return 0
     fi
 
     return 1
 }
+function plasma_playing() {
+    mpris_playing "${MPRIS_PLASMA}"
+}
+function vlc_playing() {
+    mpris_playing "${MPRIS_VLC}"
+}
 
-# check if plasma-mpris can play
+# check if mpris MediaPlayer2 can play
 function mpris_canplay() {
-    reply="$( mpris_call "Player.CanPlay" 2>/dev/null )"
-    [[ "$reply" == "true" ]] && mpris_call "Player.Metadata" | grep -e '^kde:mediaSrc' 2>&1 >/dev/null
-    if [[ $? -eq 0 ]]; then
-        return 0
+    local service="${1}"
+    local reply="$( mpris_call "${service}" "Player.CanPlay" 2>/dev/null )"
+
+    if [[ "${service}" == "${MPRIS_PLASMA}" ]]; then
+        mpris_call "Player.Metadata" | grep -e '^kde:mediaSrc' 2>&1 >/dev/null
+        if [[ "$?" -eq 0 ]] && [[ "$reply" == "true" ]]; then
+            return 0
+        fi
+    else
+        [[ "$reply" == "true" ]] && return 0
     fi
 
     return 1
+}
+function plasma_canplay() {
+    mpris_canplay "${MPRIS_PLASMA}"
+}
+function vlc_canplay() {
+    mpris_canplay "${MPRIS_VLC}"
 }
 
 
@@ -106,13 +139,20 @@ function toggle_mpd() {
     return 0
 }
 function toggle_mpris() {
-    mpris_call "Player.PlayPause" >/dev/null
+    local service="${1}"
+    mpris_call "${service}" "Player.PlayPause" >/dev/null
     if [[ $? -ne 0 ]]; then
         echo "Error: mpris toggle failed" >&2
         return 1
     fi
 
     return 0
+}
+function toggle_plasma() {
+    toggle_mpris "${MPRIS_PLASMA}"
+}
+function toggle_vlc() {
+    toggle_mpris "${MPRIS_VLC}"
 }
 
 # stop playback
@@ -126,7 +166,8 @@ function stop_mpd() {
     return 0
 }
 function stop_mpris() {
-    mpris_call "Player.Stop" >/dev/null
+    local service="${1}"
+    mpris_call "${service}" "Player.Stop" >/dev/null
     if [[ $? -ne 0 ]]; then
         echo "Error: mpris stop failed" >&2
         return 1
@@ -134,6 +175,13 @@ function stop_mpris() {
 
     return 0
 }
+function stop_plasma() {
+    stop_mpris "${MPRIS_PLASMA}"
+}
+function stop_vlc() {
+    stop_mpris "${MPRIS_VLC}"
+}
+
 
 # previous track
 function prev_mpd() {
@@ -146,7 +194,8 @@ function prev_mpd() {
     return 0
 }
 function prev_mpris() {
-    mpris_call "Player.Previous" >/dev/null
+    local service="${1}"
+    mpris_call "${service}" "Player.Previous" >/dev/null
     if [[ $? -ne 0 ]]; then
         echo "Error: mpris previous failed" >&2
         return 1
@@ -154,6 +203,13 @@ function prev_mpris() {
 
     return 0
 }
+function prev_plasma() {
+    prev_mpris "${MPRIS_PLASMA}"
+}
+function prev_vlc() {
+    prev_mpris "${MPRIS_VLC}"
+}
+
 
 # next track
 function next_mpd() {
@@ -166,13 +222,20 @@ function next_mpd() {
     return 0
 }
 function next_mpris() {
-    mpris_call "Player.Next" >/dev/null
+    local service="${1}"
+    mpris_call "${service}" "Player.Next" >/dev/null
     if [[ $? -ne 0 ]]; then
         echo "Error: mpris next failed" >&2
         return 1
     fi
 
     return 0
+}
+function next_plasma() {
+    next_mpris "${MPRIS_PLASMA}"
+}
+function next_vlc() {
+    next_mpris "${MPRIS_VLC}"
 }
 
 
@@ -181,11 +244,35 @@ function order_clients() {
     local preferred_client="$1"
     local clients=( "$preferred_client" )
 
-    for client in "mpris" "mpd"; do
+    for client in "vlc" "plasma" "mpd"; do
         [[ "$client" != "$preferred_client" ]] && clients=( ${clients[@]} "$client" )
     done
 
     echo "${clients[@]}"
+}
+
+# get all running clients
+function clients_running() {
+    local running=()
+    for client in $( order_clients ); do
+        if ${client}_isrunning; then
+            running=( ${running[@]} "$client" )
+        fi
+    done
+
+    echo "${running[@]}"
+}
+
+# get all playing clients
+function clients_playing() {
+    local playing=()
+    for client in $( clients_running ); do
+        if ${client}_playing; then
+            playing=( ${playing[@]} "$client" )
+        fi
+    done
+
+    echo "${playing[@]}"
 }
 
 # get best client for controling
@@ -193,35 +280,40 @@ function get_preferred_client() {
     local canplay="${1:-0}"
     local last_client="$( get_last_client )"
 
-    if mpd_isrunning && mpris_isrunning; then
-        # prefer the playing client if both are running
-        for client in $( order_clients ); do
-            if ${client}_playing; then
-                echo "$client"
-                return
-            fi
+    if [[ "$( clients_running | wc -w )" -gt 1 ]]; then
+        # prefer the playing client if more than one is running
+        for client in $( clients_playing ); do
+            echo "$client"
+            return 0
         done
 
         # no client is playing
         if [[ "$canplay" -ne 0 ]]; then
-            # check client which can play, last controlled first (prefers mpris if not set)
+            # check client which can play, last controlled first
+            # (prefers vlc > plasma > mpd if not set)
             for client in $( order_clients "$last_client" ); do
-                if ${client}_canplay; then
+                if ${client}_isrunning && ${client}_canplay; then
                     echo "$client"
-                    return
+                    return 0
                 fi
             done
         fi
 
-        # just try the last controlled client (or mpris if not set)
-        echo "${last_client:-mpris}"
-    else
-        # use the first running client
-        for client in $( order_clients ); do
+        # try the first running client, prefer the last controlled
+        for client in $( order_clients "$last_client" ); do
             if ${client}_isrunning; then
                 echo "$client"
-                return
+                return 0
             fi
+        done
+
+        # no client running
+        return 1
+    else
+        # use the first running client
+        for client in $( clients_running ); do
+            echo "$client"
+            return 0
         done
 
         # no client running
@@ -236,18 +328,35 @@ client=
 # handle action
 while true; do
     case $1 in
-        "mpd"|"mpris")
-            # force handling of mpd or plasma-mpris
+        "vlc"|"mpd"|"plasma")
+            # force handling of specific client
             client="$1"
             shift && continue ;;
         "t"|"toggle")
-            if [[ -z "$client" ]] && mpd_isrunning && mpris_isrunning; then
-                # both are playing, so we toggle both but prefer mpris in the next step
-                if mpd_playing && mpris_playing; then
-                    save_last_client "mpris"
-                    toggle_mpd && toggle_mpris || exit 1
+            if [[ -z "$client" ]] && [[ "$( clients_running | wc -w )" -gt 1 ]]; then
+                # More than one is running, so we toggle all that are playing but set
+                # the preferred one for the next call
+
+                if (vlc_playing && plasma_playing) || (vlc_playing && mpd_playing); then
+                    # prefer vlc over all
+                    save_last_client "vlc"
+                    toggle_vlc || exit 1
+
+                    if mpd_playing; then
+                        toggle_mpd || exit 1
+                    fi
+                    if plasma_playing; then
+                        toggle_plasma || exit 1
+                    fi
+
+                    shift && break
+                elif plasma_playing && mpd_playing; then
+                    # prefer plasma-mpris over mpd
+                    save_last_client "plasma"
+                    toggle_plasma && toggle_mpd || exit 1
                     shift && break
                 fi
+
             fi
 
             [[ -n "$client" ]] || client="$( get_preferred_client 1 )"
@@ -255,11 +364,27 @@ while true; do
             toggle_${client} || exit 1
             shift ;;
         "s"|"stop")
-            if [[ -z "$client" ]] && mpd_isrunning && mpris_isrunning; then
-                # both are playing, so we stop both but prefer mpris in the next step
-                if mpd_playing && mpris_playing; then
-                    save_last_client "mpris"
-                    stop_mpd && stop_mpris || exit 1
+            if [[ -z "$client" ]] && [[ "$( clients_running | wc -w )" -gt 1 ]]; then
+                # More than one is running, so we stop all that are playing but set
+                # the preferred one for the next call
+
+                if (vlc_playing && plasma_playing) || (vlc_playing && mpd_playing); then
+                    # prefer vlc over all
+                    save_last_client "vlc"
+                    stop_vlc || exit 1
+
+                    if mpd_playing; then
+                        stop_mpd || exit 1
+                    fi
+                    if plasma_playing; then
+                        stop_plasma || exit 1
+                    fi
+
+                    shift && break
+                elif plasma_playing && mpd_playing; then
+                    # prefer plasma-mpris over mpd
+                    save_last_client "plasma"
+                    stop_plasma && stop_mpd || exit 1
                     shift && break
                 fi
             fi
@@ -269,12 +394,20 @@ while true; do
             stop_${client} || exit 1
             shift ;;
         "p"|"prev")
-            if [[ -z "$client" ]] && mpd_isrunning && mpris_isrunning; then
-                # both are playing, so we send the signal to both
-                if mpd_playing && mpris_playing; then
-                    prev_mpd && prev_mpris || exit 1
-                    shift && break
+            if [[ -z "$client" ]] && [[ "$( clients_running | wc -w )" -gt 1 ]]; then
+                # More than one is running, so send the signal to all that are playing
+
+                if vlc_playing; then
+                    prev_vlc || exit 1
                 fi
+                if plasma_playing; then
+                    prev_plasma || exit 1
+                fi
+                if mpd_playing; then
+                    prev_mpd || exit 1
+                fi
+
+                shift && break
             fi
 
             [[ -n "$client" ]] || client="$( get_preferred_client 1 )"
@@ -282,12 +415,20 @@ while true; do
             prev_${client} || exit 1
             shift ;;
         "n"|"next")
-            if [[ -z "$client" ]] && mpd_isrunning && mpris_isrunning; then
-                # both are playing, so we send the signal to both
-                if mpd_playing && mpris_playing; then
-                    next_mpd && next_mpris || exit 1
-                    shift && break
+            if [[ -z "$client" ]] && [[ "$( clients_running | wc -w )" -gt 1 ]]; then
+                # More than one is running, so send the signal to all that are playing
+
+                if vlc_playing; then
+                    next_vlc || exit 1
                 fi
+                if plasma_playing; then
+                    next_plasma || exit 1
+                fi
+                if mpd_playing; then
+                    next_mpd || exit 1
+                fi
+
+                shift && break
             fi
 
             [[ -n "$client" ]] || client="$( get_preferred_client 1 )"
