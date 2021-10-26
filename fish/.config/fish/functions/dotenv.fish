c4df0c6dcdb91f8e14cd3fe3ba487df37c37917b
Bernd Busse <bernd@busse-net.de>
2021-10-26 12:02:07 +0200

    fish: better compatibility with dotENV parsing

--- fish/.config/fish/functions/dotenv.fish
+++ fish/.config/fish/functions/dotenv.fish
@@ -5,10 +5,18 @@ function dotenv --description 'Load environment variables from .env file'
     end
 
     for file in $envfiles
-        if test -e "$file"
-            for line in (cat "$file")
-                set -xg (echo "$line" | cut -d'=' -f1) (echo "$line" | cut -d'=' -f2-)
+        if ! test -e "$file"
+            continue
+        end
+
+        for line in (cat "$file")
+            if string match -r '^#' "$line"
+                continue
             end
+
+            set -l name (string replace -r '^export\s+' '' (echo "$line" | cut -d'=' -f1))
+            set -l value (string unescape (echo "$line" | cut -d'=' -f2-))
+            set -xg "$name" "$value"
         end
     end
 end
