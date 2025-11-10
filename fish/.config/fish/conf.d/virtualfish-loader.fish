daebfc3ed836dcc4f6325c8ef25de59622347b96
Bernd Busse <bernd@busse-net.de>
2025-11-10 20:20:34 +0100

    fish: update virtalfish to python3.13

--- fish/.config/fish/conf.d/virtualfish-loader.fish
+++ fish/.config/fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,4 @@
 set -g VIRTUALFISH_VERSION 2.5.9
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
-source /usr/lib/python3.12/site-packages/virtualfish/virtual.fish
+source /usr/lib/python3.13/site-packages/virtualfish/virtual.fish
 emit virtualfish_did_setup_plugins
