9e2b9008beece6c3554160f78926f0e09d62e629
Bernd Busse <bernd@busse-net.de>
2026-02-22 20:20:08 +0100

    Update fish config

---
+++ fish/.config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,38 @@
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+
+set --global fish_color_autosuggestion white
+set --global fish_color_cancel red
+set --global fish_color_command green
+set --global fish_color_comment yellow
+set --global fish_color_cwd cyan
+set --global fish_color_cwd_root red
+set --global fish_color_end magenta
+set --global fish_color_error red
+set --global fish_color_escape yellow
+set --global fish_color_history_current --bold
+set --global fish_color_host yellow
+set --global fish_color_host_remote yellow
+set --global fish_color_match --background=brblue
+set --global fish_color_normal normal
+set --global fish_color_operator blue
+set --global fish_color_param cyan
+set --global fish_color_quote yellow
+set --global fish_color_redirection magenta
+set --global fish_color_search_match white --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user green
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_prefix white --bold --underline=single
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_selected_background --reverse
+set --global fish_pager_color_selected_completion 
+set --global fish_pager_color_selected_description 
+set --global fish_pager_color_selected_prefix 
